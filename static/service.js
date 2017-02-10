c58a5b5b5a96cb4c5680d5aef06eaf5edd25068a
Adonis <lauweeking@gmail.com>
2017-02-10 16:56:44 +0800

    test

---
+++ static/service.js
@@ -0,0 +1,22 @@
+this.addEventListener('install', function(event) {
+  console.log('a')
+  event.waitUntil(
+    caches.open('v1').then(function(cache) {
+      return cache.addAll([
+        'static/css/index.css'
+      ]);
+    })
+  );
+});
+
+this.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(resp) {
+        console.log(resp);
+        return resp || fetch(event.request).then(function(response) {
+          return response; 
+        });
+      })
+  );
+});
